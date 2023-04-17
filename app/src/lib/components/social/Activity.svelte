a9582d15e4c6adf351d4d94696fe1c13779672eb
Hunter Perrin <hperrin@gmail.com>
2023-04-16 20:13:49 -0700

    feat: add social actor and activity components

---
+++ app/src/lib/components/social/Activity.svelte
@@ -0,0 +1,25 @@
+<Paper>
+  <Subtitle>
+    <span title={new Date(actvitiy.cdate || 0).toLocaleString()}
+      ><RelativeDate date={actvitiy.cdate} /></span
+    >
+  </Subtitle>
+  <Content>
+    <pre style="max-width: 100%; overflow-x: auto;">{JSON.stringify(
+        actvitiy,
+        null,
+        2
+      )}</pre>
+  </Content>
+</Paper>
+
+<script lang="ts">
+  import Paper, { Subtitle, Content } from '@smui/paper';
+  import RelativeDate from '$lib/components/RelativeDate.svelte';
+  import type {
+    SocialActivity,
+    SocialActivityData,
+  } from '$lib/entities/SocialActivity';
+
+  export let actvitiy: SocialActivity & SocialActivityData;
+</script>
---
+++ app/src/lib/components/social/Actor.svelte
@@ -0,0 +1,22 @@
+<Paper>
+  <Subtitle>
+    <span title={new Date(actor.cdate || 0).toLocaleString()}
+      ><RelativeDate date={actor.cdate} /></span
+    >
+  </Subtitle>
+  <Content>
+    <pre style="max-width: 100%; overflow-x: auto;">{JSON.stringify(
+        actor,
+        null,
+        2
+      )}</pre>
+  </Content>
+</Paper>
+
+<script lang="ts">
+  import Paper, { Subtitle, Content } from '@smui/paper';
+  import RelativeDate from '$lib/components/RelativeDate.svelte';
+  import type { SocialActor, SocialActorData } from '$lib/entities/SocialActor';
+
+  export let actor: SocialActor & SocialActorData;
+</script>
